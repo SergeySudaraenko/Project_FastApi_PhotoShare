842e8ca5a15515b3cdc901c956501ed339f90734
Serhii Sudarenko <159451661+SergeySudaraenko@users.noreply.github.com>
2024-09-02 19:10:04 +0200

    Revert "Feature/tags"

--- src/schemas/tags.py
+++ src/schemas/tags.py
@@ -3,41 +3,14 @@ from pydantic import BaseModel, ConfigDict
 from src.schemas.search import TagBase
 
 
-
 class TagCreate(BaseModel):
     name: str
 
     model_config = ConfigDict(from_attributes=True)
 
 
-
-
-class TagUpdate(TagBase):
-    pass
-
-
-class TagResponse(TagBase):
-
-
+class TagResponse(BaseModel):
     id: int
     name: str
 
-    class Config:
-        orm_mode = True
-
-class PhotoBase(BaseModel):
-    pass
-
-class PhotoResponse(PhotoBase):
-    id: int
-    url: str
-
-
-
-
-
-
-
-
-
-
+    model_config = ConfigDict(from_attributes=True)
