dcdd2b5773d6851e6e871e4d0f816b6fdb3a1cba
Ksushondrik <ksushondrik369@gmail.com>
2024-09-06 11:52:33 +0300

    the database connection settings have been changed

--- src/config/config.py
+++ src/config/config.py
@@ -3,20 +3,22 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_URL: str = "postgresql+asyncpg://postgres:567234@localhost:5432/ProjectDB"
-    SECRET_KEY_JWT: str = "1234567890"
-    ALGORITHM: str = "HS256"
-    MAIL_USERNAME: str = "postgresmail.com"
-    MAIL_PASSWORD: str = "postgres"
-    MAIL_FROM: str = "postgres"
-    MAIL_PORT: int = 567234
-    MAIL_SERVER: str = "postgres"
-    # REDIS_DOMAIN: str = "localhost"
-    # REDIS_PORT: int = 6379
-    # REDIS_PASSWORD: str = None
-    CLOUDINARY_NAME: str
-    CLOUDINARY_API_KEY: str
-    CLOUDINARY_API_SECRET: str
+    PG_DB = "koyebdb"
+    PG_USER = "koyeb-adm"
+    PG_PASSWORD = "v6sNjAnxio5q"
+    PG_PORT = "5432"
+    PG_DOMAIN = "ep-delicate-credit-a285zbki.eu-central-1.pg.koyeb.app"
+    DB_URL = f"postgresql+asyncpg://${PG_USER}:${PG_PASSWORD}@${PG_DOMAIN}:${PG_PORT}/${PG_DB}"
+    SECRET_KEY_JWT = 1234567890
+    ALGORITHM = "HS256"
+    MAIL_USERNAME = "vlad_bb@meta.ua"
+    MAIL_PASSWORD = "tQM2JULusy"
+    MAIL_FROM = "vlad_bb@meta.ua"
+    MAIL_PORT = 465
+    MAIL_SERVER = "smtp.meta.ua"
+    CLOUDINARY_NAME = "dpc5fcmq5"
+    CLOUDINARY_API_KEY = 951437173691459
+    CLOUDINARY_API_SECRET = "6irfVmNhHCvEDnMjaGy4DRSjcpk"
 
     @classmethod
     @field_validator("ALGORITHM")
@@ -25,7 +27,7 @@ class Settings(BaseSettings):
             raise ValueError("Algorithm must be HS256 or HS512")
         return v
 
-    model_config = ConfigDict(extra="ignore", env_file=".env", env_file_encoding="utf-8")   # noqa
+    model_config = ConfigDict(extra="ignore", env_file=".env", env_file_encoding="utf-8")  # noqa
 
 
 settings = Settings()
